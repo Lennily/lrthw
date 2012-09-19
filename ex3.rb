5ca106926ff5ac677c0a9431c7379696de947ea1
Programmer <ehinell@gmail.com>
2012-09-19 15:40:10 +0300

    adding the ex3.rb file

---
+++ ex3.rb
@@ -0,0 +1,47 @@
+# outputs the string
+puts "I will now count my chickens:"
+
+# outputs the string and the result of solving the math problem
+puts "Hens", 25 + 30 / 6
+puts "Roosters", 100 - 25 * 3 % 4
+
+# outputs the string
+puts "Now I will count the eggs:"
+
+# outputs the result of solving the math problem
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# outputs the string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# outputs the result of comparision - false
+puts 3 + 2 < 5 - 7
+
+# outputs the string and the result of solving the math problem
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+# outputs the string
+puts "Oh, that's why it's false."
+
+# outputs the string
+puts "How about some more."
+
+# outputs the string and the result of comparision
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
+
+
+
+=begin
++ plus does addition
+- minus does subtraction
+/ slash does division
+* asterisk does multiplication
+% percent does modulo
+< less-than compares, less than
+> greater-than compares, greater than
+<= less-than-equal compares less than equal
+>+ greater-than-equal compares greater than equal
+=end
