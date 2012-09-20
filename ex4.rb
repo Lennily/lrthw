e118caf7daaf36c57496d2c6fd33b2a45111f612
Programmer <ehinell@gmail.com>
2012-09-20 16:26:10 +0300

    added ex4.rb

--- ex4.rb
+++ ex4.rb
@@ -16,7 +16,7 @@ carpool_capacity = cars_driven * space_in_a_car
 # assigning a variable with math problem
 average_passengers_per_car = passengers / cars_driven
 
-# outputs the strings with variables
+# outputs the strings with variables :)
 puts "There are #{cars} cars available."
 puts "There are only #{drivers} drivers available."
 puts "There will be #{cars_not_driven} empty cars today."
